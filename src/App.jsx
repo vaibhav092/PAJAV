33b969178529b2a9c9df70e4ddfc03a2d2cab28c
Vaibhav092 <vaibhavcr7092@gmail.com>
2025-02-05 21:36:27 +0530

    Remove

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+    return (
+    <div className="min-h-screen bg-green-100 text-green-900 flex justify-center items-center">
+        <h1 className="text-5xl font-bold text-green-700">PAJAV Society</h1>
+    </div>
+
+    )
+}
+export default App
